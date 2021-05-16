0d266b1be1bbdd6068d242b1bc850072f09ec704
Aman Priyanshu <aman.priyanshu@learner.manipal.edu>
2021-05-16 11:51:07 +0530

    Update model_helper.py

--- backend/model_helper.py
+++ backend/model_helper.py
@@ -1,17 +1,23 @@
 import torch
 import numpy as np
+from model_generator import generate_model
 
-def get_weights(self, dtype=np.float32):
-		precision = self.precision
+class ModelManager:
+	def __init__(self, model_path='./models/model.pt'):
+		self.model = generate_model()
+		self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+		self.model.load_state_dict(torch.load(model_path, map_location=self.device))
+
+	def get_weights(self, dtype=np.float32):
 		weights = []
 		for layer in self.model:
 			try:
-				weights.append([np.around(layer.weight.detach().numpy().astype(dtype), decimals=precision), np.around(layer.bias.detach().numpy().astype(dtype), decimals=precision)])
+				weights.append([np.around(layer.weight.detach().numpy().astype(dtype)), np.around(layer.bias.detach().numpy().astype(dtype))])
 			except:
 				continue
 		return np.array(weights)
   
-def set_weights(self, weights):
+	def set_weights(self, weights):
 		index = 0
 		for layer_no, layer in enumerate(self.model):
 			try:
@@ -22,7 +28,7 @@ def set_weights(self, weights):
 			except:
 				continue
         
-def average_weights(self, all_weights):
+	def average_weights(self, all_weights):
 		all_weights = np.array(all_weights)
 		all_weights = np.mean(all_weights, axis=0)
 		all_weights = [[torch.from_numpy(i[0].astype(np.float32)), torch.from_numpy(i[1].astype(np.float32))] for i in all_weights]
