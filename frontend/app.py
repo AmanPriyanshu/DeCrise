855346eefcb972152b7d3c2fdc1e3773bcdc503a
Aman Priyanshu <aman.priyanshu@learner.manipal.edu>
2021-05-16 20:33:41 +0530

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import time
 from test import test
+import os
 
 st.markdown("""
 <style>
@@ -17,6 +18,9 @@ st.markdown('<p class="color-font">DeCrise</p>', unsafe_allow_html=True)
 
 st.subheader("Dataset")
 data_file = st.file_uploader("Upload CSV", type=['csv'])
+
+st.write(str(os.listdir()))
+
 if st.button("Process"):
     if data_file is not None:
         df = pd.read_csv(data_file)
